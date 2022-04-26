ef4bf4c2f53303ff2791e75a4bbb4912ebd9b474
SoftwareRat <jaguar5018@gmail.com>
2022-04-26 10:45:07 +0200

    - Fixing winget download - Don't force NuGet for all users - Fixed ExecutionAlias to correct binary

--- Install-WinGet.ps1
+++ Install-WinGet.ps1
@@ -25,18 +25,20 @@ IF ((Get-MpComputerStatus).AntivirusEnabled) {
 
 # Install NtObjectManager module
 Set-PSRepository -Name "PSGallery" -InstallationPolicy 'Trusted'
-Install-PackageProvider -Name 'NuGet' -Scope 'AllUsers' -Force
+Install-PackageProvider -Name 'NuGet' -Force
 Install-Module -Name 'NtObjectManager' -Force 
 
 # Getting links to download packages
 $vclibs = Invoke-WebRequest -Uri "https://store.rg-adguard.net/api/GetFiles" -Method "POST" -ContentType "application/x-www-form-urlencoded" -Body "type=PackageFamilyName&url=Microsoft.VCLibs.140.00_8wekyb3d8bbwe&ring=RP&lang=en-US" -UseBasicParsing | Foreach-Object Links | Where-Object outerHTML -match "Microsoft.VCLibs.140.00_.+_x64__8wekyb3d8bbwe.appx" | Foreach-Object href
 $vclibsuwp = Invoke-WebRequest -Uri "https://store.rg-adguard.net/api/GetFiles" -Method "POST" -ContentType "application/x-www-form-urlencoded" -Body "type=PackageFamilyName&url=Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe&ring=RP&lang=en-US" -UseBasicParsing | Foreach-Object Links | Where-Object outerHTML -match "Microsoft.VCLibs.140.00.UWPDesktop_.+_x64__8wekyb3d8bbwe.appx" | Foreach-Object href
-$winget = ((Invoke-RestMethod "https://api.github.com/repos/microsoft/winget-cli/releases/latest").assets.browser_download_url) -like "*.msixbundle"
+$winget = ((Invoke-RestMethod 'https://api.github.com/repos/microsoft/winget-cli/releases/latest').assets.browser_download_url) -like "*.msixbundle"
+$UIxaml = (Invoke-WebRequest -Uri "https://store.rg-adguard.net/api/GetFiles" -Method "POST" -ContentType "application/x-www-form-urlencoded" -Body "type=PackageFamilyName&url=Microsoft.UI.Xaml.2.7_8wekyb3d8bbwe&ring=RP&lang=en-US" -UseBasicParsing | Foreach-Object Links | Where-Object outerHTML -match "Microsoft.UI.Xaml.2.7_.+_x64__8wekyb3d8bbwe.appx" | Foreach-Object href)
 
 # Downloading packages
-Invoke-WebRequest -Uri $vclibs -OutFile $ENV:TEMP\Microsoft.VCLibs.140.00_8wekyb3d8bbwe.appx
-Invoke-WebRequest -Uri $vclibsuwp -OutFile $ENV:TEMP\Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx
-Invoke-WebRequest -Uri $winget -OutFile $ENV:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri "$vclibs" -OutFile $ENV:TEMP\Microsoft.VCLibs.140.00_8wekyb3d8bbwe.appx
+Invoke-WebRequest -Uri "$vclibsuwp" -OutFile $ENV:TEMP\Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx
+Invoke-WebRequest -Uri "$UIxaml" -OutFile Microsoft.UI.Xaml.2.7_8wekyb3d8bbwe.appx
+Invoke-WebRequest -Uri "$winget" -OutFile $ENV:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
 # Installing packages
 Add-AppxPackage -Path $ENV:TEMP\Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx
@@ -46,7 +48,7 @@ Add-AppxPackage -Path $ENV:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msix
 # Create reparse point
 Import-Module -Name NtObjectManager
 $installationPath = (Get-AppxPackage Microsoft.DesktopAppInstaller).InstallLocation
-Set-ExecutionAlias -Path "C:\Windows\System32\winget.exe" -PackageName "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe" -EntryPoint "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe!winget" -Target "$installationPath\AppInstallerCLI.exe" -AppType Desktop -Version 3
+Set-ExecutionAlias -Path "C:\Windows\System32\winget.exe" -PackageName "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe" -EntryPoint "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe!winget" -Target "$installationPath\winget.exe" -AppType Desktop -Version 3
 explorer.exe "shell:appsFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe!winget"
 
 ## Checking is Microsoft Defender Antivirus is enabled
